d85d021d768fc8cd8acf8e9d90ea5efacdf2ee18
Matt Post <post@cs.jhu.edu>
2019-10-08 16:17:42 -0400

    reads tab-delimted trees on STDIN, computes kernel

--- tree-kernel/compare-trees.cpp
+++ tree-kernel/compare-trees.cpp
@@ -1,7 +1,7 @@
 /*
 Copyright (c) 2019, Matt Post
 
-Takes two tab-delimited trees per line on STDIN, computes the SST kernel.
+Takes tab-delimited trees on STDIN, computes kernel between all pairs.
 */
 
 #include <string>
@@ -12,24 +12,42 @@ Takes two tab-delimited trees per line on STDIN, computes the SST kernel.
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-   string line;
-   int lineno = 0;
-   while (getline(cin, line)) {
-     lineno++;
-     string::size_type tabpos = line.find("\t");
-     if (tabpos == string::npos) {
-       cerr << "[" << lineno << "] No tab found" << endl;
-       cout << endl;
-       continue;
-     }
+  string line;
+  int lineno = 0;
+  while (getline(cin, line)) {
+    lineno++;
 
-     string tree1_str = line.substr(0, tabpos);
-     string tree2_str = line.substr(tabpos + 1);
-     double value = kernel_value(tree1_str, tree2_str, true);
-     cout << value << endl;
-   }
+    // build a list of the trees
+    vector<string> trees;
+    string::size_type tabpos, startpos = 0;
+    while ((tabpos = line.find("\t", startpos)) != string::npos) {
+      string tree = line.substr(startpos, tabpos - startpos);
+      trees.push_back(tree);
+      startpos = tabpos + 1;
+    }
+    if (startpos != 0)
+      trees.push_back(line.substr(startpos));
 
-   return 0;
+    double score = 0;
+    for (vector<string>::iterator tree1 = trees.begin();
+         tree1 != trees.end();
+         tree1++) {
+      for (vector<string>::iterator tree2 = tree1 + 1;
+           tree2 != trees.end();
+           tree2++) {
+
+        double overlap = kernel_value(*tree1, *tree2);
+        score += overlap;
+        // cout << "CMP\n" << *tree1 << endl << *tree2 << endl;
+        // cout << z << endl << endl;
+        // cout << overlap << " / " << z << " = " << overlap / z << endl;
+        // cout << overlap << endl;
+      }
+    }
+    cout << score << endl;
+  }
+
+  return 0;
 }
